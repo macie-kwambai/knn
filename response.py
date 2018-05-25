faf329cd4c2275a22b141ad8b03ed1a99ec2db23
macie-kwambai <maciekwambai@gmail.com>
2018-05-25 13:51:00 +0300

    First commit

---
+++ response.py
@@ -0,0 +1,26 @@
+import operator
+def getResponse(neighbors):
+	classVotes = {}
+	for x in range(len(neighbors)):
+		response = neighbors[x][-1]
+		if response in classVotes:
+			classVotes[response] += 1
+		else:
+			classVotes[response] = 1
+	sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+	return sortedVotes[0][0]
+def getAccuracy(testSet, predictions):
+	correct = 0
+	for x in range(len(testSet)):
+		if testSet[x][-1] is predictions[x]:
+			correct += 1
+	return (correct/float(len(testSet))) * 100.0
+
+
+testSet = [[1,1,1,'a'], [2,2,2,'a'], [3,3,3,'b']]
+predictions = ['a', 'a', 'a']
+accuracy = getAccuracy(testSet, predictions)
+print(accuracy)
+
+
+
